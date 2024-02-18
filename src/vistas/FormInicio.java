690886eef58d0afc127097ceb68f3aa4db35d0a3
Johnny Benitez <johnnybeniitez@gmail.com>
2024-02-18 16:37:47 -0300

    Modificaciones al JFrame FormInicio
    tamaño del panel.. layout null y demás cositas

--- src/vistas/FormInicio.java
+++ src/vistas/FormInicio.java
@@ -5,6 +5,7 @@ public class FormInicio extends javax.swing.JFrame {
 
     
     public FormInicio() {
+        this.setUndecorated(true);
         initComponents();
         this.setResizable(false);
         this.setLocationRelativeTo(null);
@@ -15,17 +16,29 @@ public class FormInicio extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        PanelFondo = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        PanelFondo.setBackground(new java.awt.Color(14, 19, 49));
+        PanelFondo.setPreferredSize(new java.awt.Dimension(416, 315));
+        PanelFondo.setLayout(null);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(137, 137, 137)
+                .addComponent(PanelFondo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(109, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(100, 100, 100)
+                .addComponent(PanelFondo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(78, Short.MAX_VALUE))
         );
 
         pack();
@@ -34,5 +47,6 @@ public class FormInicio extends javax.swing.JFrame {
    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel PanelFondo;
     // End of variables declaration//GEN-END:variables
 }
